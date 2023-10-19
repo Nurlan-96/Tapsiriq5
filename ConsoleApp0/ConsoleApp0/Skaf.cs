2291d460cc2ebae30bb5e1d04e7ede452d30b87f
Nurlan <nurlanea@code.edu.az>
2023-10-19 20:17:25 +0400

    upload

---
+++ ConsoleApp0/ConsoleApp0/Skaf.cs
@@ -0,0 +1,19 @@
+﻿int QapiA = 3;
+int QapiB = 7;
+int QapiC = 10;
+
+int SkafA  = 2;
+int SkafB =  8;
+int SkafC = 10;
+
+// A=en B=uzunluq C=hundurluk
+
+if ((QapiC >= SkafC && QapiB >= SkafB) || (QapiC >= SkafC && QapiB >= SkafA) || (QapiB >= SkafA && QapiC >= SkafB) || (QapiB >= SkafC && QapiC >= QapiA) || (QapiB >= SkafC && QapiC >= SkafB) || (QapiB >= SkafC && QapiC >= SkafA))
+{
+    Console.WriteLine("Kecir");
+}
+
+else
+{
+    Console.WriteLine("Kecmir");
+}
---
+++ ikiucreqemli/ikiucreqemli/Program.cs
@@ -0,0 +1,16 @@
+﻿float num1 = 500;
+float num2 = 500;
+float sum = num1 + num2;
+
+if ((sum / 10 >= 1) && (sum/ 10 < 10))
+{
+    Console.WriteLine("Ikiededlidir");
+}
+else if ((sum / 100 >= 1) && (sum / 100 < 10))
+{
+    Console.WriteLine("Ucededlidir");
+}
+else
+{
+    Console.WriteLine("nezere alinmayıb");
+}
